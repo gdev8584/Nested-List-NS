e76c58dbd448e5591052ab4ab9248c2a3f36c62d
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-10-01 19:57:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import { StateDetails } from "./State";
 
 // Do not alter the states const and values inside it.
 const states = [
